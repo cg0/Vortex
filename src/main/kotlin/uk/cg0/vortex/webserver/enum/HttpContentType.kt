1ee435f849c0b1ae275ae1cbb48feda1d67574e3
Connor Graham <thecg1997@gmail.com>
2021-06-02 23:08:28 +0100

    Fix typo in HttpContentType enum

--- src/main/kotlin/uk/cg0/vortex/webserver/enum/HttpContentType.kt
+++ src/main/kotlin/uk/cg0/vortex/webserver/enum/HttpContentType.kt
@@ -3,7 +3,7 @@ package uk.cg0.vortex.webserver.enum
 enum class HttpContentType(private val value: String) {
     APPLICATION_OCTET_STREAM("application/octet-stream"),
     APPLICATION_X_WWW_FORM_URLENCODED("application/x-www-form-urlencoded"),
-    TEXT_PLAN("text/plain"),
+    TEXT_PLAIN("text/plain"),
     TEXT_HTML("text/html"),
     IMAGE_PNG("image/png");
 
