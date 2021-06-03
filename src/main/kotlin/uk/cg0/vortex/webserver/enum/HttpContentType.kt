b72510efd5d9eb3348ac3d8b9472c44fd093a9c1
Connor Graham <thecg1997@gmail.com>
2021-06-03 13:52:15 +0100

    Response: Allow file responses

--- src/main/kotlin/uk/cg0/vortex/webserver/enum/HttpContentType.kt
+++ src/main/kotlin/uk/cg0/vortex/webserver/enum/HttpContentType.kt
@@ -5,9 +5,23 @@ enum class HttpContentType(private val value: String) {
     APPLICATION_X_WWW_FORM_URLENCODED("application/x-www-form-urlencoded"),
     TEXT_PLAIN("text/plain"),
     TEXT_HTML("text/html"),
-    IMAGE_PNG("image/png");
+    IMAGE_PNG("image/png"),
+    IMAGE_JPG("image/jpg"),
+    IMAGE_JPEG("image/jpeg");
 
     override fun toString(): String {
         return this.value
+    }
+
+    companion object {
+        fun getByValue(value: String): HttpContentType {
+            for (contentType in HttpContentType.values()) {
+                if (contentType.value == value) {
+                    return contentType
+                }
+            }
+
+            return APPLICATION_OCTET_STREAM
+        }
     }
 }
--- src/main/kotlin/uk/cg0/vortex/webserver/objects/Response.kt
+++ src/main/kotlin/uk/cg0/vortex/webserver/objects/Response.kt
@@ -4,7 +4,9 @@ import uk.cg0.vortex.template.Template
 import uk.cg0.vortex.webserver.enum.HttpContentType
 import uk.cg0.vortex.webserver.enum.HttpStatus
 import java.io.ByteArrayOutputStream
+import java.io.File
 import java.nio.charset.Charset
+import java.nio.file.Files
 
 data class Response(var statusCode: HttpStatus,
                     val headers: HashMap<String, String>,
@@ -20,5 +22,11 @@ data class Response(var statusCode: HttpStatus,
     fun renderView(template: Template) {
         outputStream.write(template.render().toByteArray(Charset.forName("UTF-8")))
         contentType = HttpContentType.TEXT_HTML
+    }
+
+    fun serveFile(file: File) {
+        val mimeType = Files.probeContentType(file.toPath())
+        contentType = HttpContentType.getByValue(mimeType)
+        outputStream.write(file.readBytes())
     }
 }
